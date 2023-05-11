e4c4420c2e4d7cf53390aaa0b96fd48fdfca6f33
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-05-11 10:44:04 +0200

    added more feature like too low or too high

--- main.py
+++ main.py
@@ -1,52 +1,67 @@
 import random
 
-
-def guess_the_number():
-    print("Welcome to Guess the Number!")
-    print("Try to guess the secret number within the chosen range.")
-  
+# Define function to get player input and validate it
+def get_input(prompt, type_=None, min_=None, max_=None, range_=None):
     while True:
-        difficulty = input("Select a difficulty level (easy, medium, hard): ").lower()
-        
-        if difficulty not in ['easy', 'medium', 'hard']:
-            print("Invalid difficulty level. Please try again.")
-            continue
-
-        if difficulty == 'easy':
-            low = 1
-            high = 50
-            attempts_allowed = 10
-        elif difficulty == 'medium':
-            low = 1
-            high = 100
-            attempts_allowed = 7
-        else:
-            low = 1
-            high = 200
-            attempts_allowed = 5
-
-        print(f"You've selected {difficulty} level. Guess a number between {low} and {high}.")
-
-        number = random.randint(low, high)
-        attempts = 0
-
-        while attempts < attempts_allowed:
-            guess = int(input("Enter your guess: "))
-            attempts += 1
-
-            if guess < number:
-                print("Too low! Try again.")
-            elif guess > number:
-                print("Too high! Try again.")
+        try:
+            value = type_(input(prompt))
+            if min_ is not None and value < min_:
+                print(f"Value must be greater than or equal to {min_}.")
+            elif max_ is not None and value > max_:
+                print(f"Value must be less than or equal to {max_}.")
+            elif range_ is not None and value not in range(*range_):
+                print(f"Value must be between {range_[0]} and {range_[1]}.")
             else:
-                print(f"Congratulations! You guessed the number in {attempts} attempts!")
-                break
+                return value
+        except ValueError:
+            print("Invalid input.")
 
-        if attempts >= attempts_allowed:
-            print(f"Game over! The number was {number}. Better luck next time!")
+# Define function to play the game
+def play_game():
+    print("Welcome to Guess the Number!")
+    
+    # Get difficulty level from the player
+    level = get_input("Select a difficulty level (1 = easy, 2 = medium, 3 = hard): ",
+                      type_=int, min_=1, max_=3)
+    
+    # Define difficulty levels
+    if level == 1:
+        attempts = 10
+        low = 1
+        high = 50
+    elif level == 2:
+        attempts = 7
+        low = 1
+        high = 100
+    else:
+        attempts = 5
+        low = 1
+        high = 200
+    
+    # Generate random number
+    secret_number = random.randint(low, high)
+    
+    # Loop until the player guesses the number or runs out of attempts
+    for attempt in range(1, attempts + 1):
+        print(f"You have {attempts - attempt + 1} attempts remaining.")
+        guess = get_input(f"Guess a number between {low} and {high}: ",
+                          type_=int, range_=(low, high))
+        if guess == secret_number:
+            print(f"Congratulations! You guessed the number in {attempt} attempts!")
+            return True
+        elif guess < secret_number:
+            print("Too low! Try again.")
+        else:
+            print("Too high! Try again.")
+    
+    # If the player runs out of attempts, reveal the secret number and end the game
+    print(f"Game over! The secret number was {secret_number}.")
+    return False
 
-        play_again = input("Do you want to play again? (yes/no): ").lower()
-        if play_again != 'yes':
-            break
-
-guess_the_number()
+# Main game loop
+while True:
+    play_game()
+    response = input("Do you want to play again? (y/n) ")
+    if response.lower() != "y":
+        print("Thanks for playing!")
+        break
