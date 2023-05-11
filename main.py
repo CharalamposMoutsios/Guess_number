7bb64312490cedeb6ea377361877d3655bc9745e
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-05-11 10:36:26 +0200

    add easy medium hard level

--- main.py
+++ main.py
@@ -2,22 +2,50 @@ import random
 
 def guess_the_number():
     print("Welcome to Guess the Number!")
-    print("I'm thinking of a number between 1 and 100.")
-    print("Try to guess the number in the fewest attempts possible.")
-  
-    number = random.randint(1, 100)
-    attempts = 0
+    print("Try to guess the secret number within the chosen range.")
   
     while True:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
+        difficulty = input("Select a difficulty level (easy, medium, hard): ").lower()
+        
+        if difficulty not in ['easy', 'medium', 'hard']:
+            print("Invalid difficulty level. Please try again.")
+            continue
 
-        if guess < number:
-            print("Too low! Try again.")
-        elif guess > number:
-            print("Too high! Try again.")
+        if difficulty == 'easy':
+            low = 1
+            high = 50
+            attempts_allowed = 10
+        elif difficulty == 'medium':
+            low = 1
+            high = 100
+            attempts_allowed = 7
         else:
-            print(f"Congratulations! You guessed the number in {attempts} attempts!")
+            low = 1
+            high = 200
+            attempts_allowed = 5
+
+        print(f"You've selected {difficulty} level. Guess a number between {low} and {high}.")
+
+        number = random.randint(low, high)
+        attempts = 0
+
+        while attempts < attempts_allowed:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < number:
+                print("Too low! Try again.")
+            elif guess > number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts!")
+                break
+
+        if attempts >= attempts_allowed:
+            print(f"Game over! The number was {number}. Better luck next time!")
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
             break
 
 guess_the_number()
